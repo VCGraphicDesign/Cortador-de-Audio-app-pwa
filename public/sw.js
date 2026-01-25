83a22e3825ac776dc1899a3e7dc6e5db35c8a69b
VCGraphicDesign <g.d.chile@gmail.com>
2026-01-25 18:51:50 -0300

    z

--- public/sw.js
+++ public/sw.js
@@ -1,10 +1,22 @@
-// Minimal Service Worker to enable PWA installability
 const CACHE_NAME = 'audiocutter-pro-v1';
 
 self.addEventListener('install', (event) => {
+    event.waitUntil(
+        caches.open(CACHE_NAME).then((cache) => {
+            return cache.addAll(['/']);
+        })
+    );
     self.skipWaiting();
 });
 
+self.addEventListener('activate', (event) => {
+    event.waitUntil(self.clients.claim());
+});
+
 self.addEventListener('fetch', (event) => {
-    event.respondWith(fetch(event.request));
+    event.respondWith(
+        fetch(event.request).catch(() => {
+            return caches.match(event.request);
+        })
+    );
 });
