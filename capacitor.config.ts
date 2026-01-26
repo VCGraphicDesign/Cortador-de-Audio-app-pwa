b796b0964d0c3297c9bfcfb5f69b0d487ca7b16b
VCGraphicDesign <g.d.chile@gmail.com>
2026-01-26 08:13:11 -0300

    3

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.vcgraphic.audiocutter',
+  appName: 'Cortador de Audio Pro',
+  webDir: 'dist'
+};
+
+export default config;
